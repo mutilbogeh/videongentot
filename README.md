bcee7f5f51696ea95c5a1917b70cc29bcd6dd1cd
mutilbogeh <mutilbogeh@gmail.com>
2025-02-17 17:31:13 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 A live demo of the project is available at [https://videongentot.pages.dev](https://videongentot.pages.dev/).
-Fork the [repository](https://github.com/mutilbogeh/bocilsdcolmek) on GitHub.
+Fork the [repository](https://github.com/mutilbogeh) on GitHub.
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,20 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: `Video Ngentot - Video Bokep Indo Viral Terbaru`,
-    description: `Video Ngentot Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
     metadataBase: new URL("https://videongentot.pages.dev"),
     alternates: {
         canonical: `/`,
     },
     openGraph: {
-        title: `Video Ngentot - Video Bokep Indo Viral Terbaru`,
-        description: `Video Ngentot Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         url: `/`,
         type: `website`,
     },
@@ -34,8 +35,8 @@ export default function RootLayout({
     const jsonLd = {
         "@context": "https://schema.org",
         "@type": "Organization",
-        "name": `Video Ngentot`,
-        "description": `Video Ngentot Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         "logo": "https://videongentot.pages.dev/favicon.ico",
         "url": "https://videongentot.pages.dev",
         "founder":{
@@ -43,25 +44,14 @@ export default function RootLayout({
             "name":"admin",
             "url":"https://videongentot.pages.dev"
             },
-            "foundingDate":"2024-06-09"
-        }
-        const jsonLd1 = {
-            "@context": "https://schema.org",
-        "@type": "WebPage",
-        "name": `Video Ngentot`,
-        "url": "https://videongentot.pages.dev",
-        "description": `Video Ngentot Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
-        "image": "https://videongentot.pages.dev/favicon.ico",
-        "potentialAction":{
-            "@type":"ReadAction",
-            "target":"https://videongentot.pages.dev"}
+            "foundingDate":"2024-06-10"
         }
         const jsonLd2 = {
             "@context": "https://schema.org",
         "@type": "WebSite",
-        "name": `Video Ngentot`,
+        "name": `${SITENAME}`,
         "url": "https://videongentot.pages.dev",
-        "description": `Video Ngentot Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
         "potentialAction": { 
         "@type": "SearchAction", 
           "target": "https://videongentot.pages.dev/?q={search_term}", 
@@ -79,10 +69,6 @@ export default function RootLayout({
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
         />
         <script
           type="application/ld+json"
